d9b6ed2366ef2bdf9579bbbb346d21031c6a6518
Thomas Karpiniec <tk@1.21jiggawatts.net>
2015-09-12 22:28:02 +1000

    Implement minor issue assignment

--- issueassignment.js
+++ issueassignment.js
@@ -34,7 +34,68 @@ function assignMajorIssues() {
 function assignMinorIssues() {
 	var g = window.game;
 	
+	// Possibly enough the most efficient approach
+	// But it does ensure it's not egregiously unfair
+
+	var totalMinorIssueAssignments = g.players.length * MINOR_ISSUES_PER_PLAYER;
 	
+	// Get an evenly shuffled stream of issue indexes at least totalMinorIssueAssignments at length
+	var randomisedIssues = [];
+	var issuesCount = g.issues.length;
+	var shufflesRequired = totalMinorIssueAssignments / issuesCount + 1;
+	for (var i = 0; i < shufflesRequired; i++) {
+		var issueIndexes = [];
+		for (var j = 0; j < g.issues.length; j++) {
+			issueIndexes.push(j);
+		}
+		issueIndexes = shuffleArray(issueIndexes);
+		randomisedIssues = randomisedIssues.concat(issueIndexes);
+	}
+	
+	var inFavourAssignments = {};
+	var notInFavourAssignments = {};
+	for (var i = 0; i < g.issues.length; i++) {
+		inFavourAssignments[i] = 0;
+		notInFavourAssignments[i] = 0;
+	}
+	
+	// Now give each player the required number of issues from the random stream
+	// For each one assigned, make sure it corrects any favour imbalance in previous assignments for the same issues
+	for (var p = 0; p < g.players.length; p++) {
+		var player = g.players[p];
+		for (var i = 0; i < MINOR_ISSUES_PER_PLAYER; i++) {
+			var index = p * MINOR_ISSUES_PER_PLAYER + i;
+			var issueIndex = randomisedIssues[index];
+			
+			// By default, random
+			var inFavour = Math.floor(Math.random() + 0.5) == 1;
+			// But if there have been more in favour than not in favour, be not in favour
+			if (inFavourAssignments[issueIndex] > notInFavourAssignments[issueIndex]) {
+				inFavour = false;
+			}
+			// Or if it's the other way around, be not in favour
+			if (inFavourAssignments[issueIndex] < notInFavourAssignments[issueIndex]) {
+				inFavour = true;
+			}
+			
+			// Update the counts for the current item
+			if (inFavour) {
+				inFavourAssignments[issueIndex]++;
+			} else {
+				notInFavourAssignments[issueIndex]++;
+			}
+			
+			// Finally, create an assignment
+			var assignedIssue = new AssignedIssue(issueIndex, MINOR_ISSUE_WEIGHT, inFavour);
+			player.assignedIssues.push(assignedIssue);
+		}
+	}
+}
+
+
+function isPlayerInFavourOfIssue(playerIndex, issueIndex) {
+	// TODO do this to make our life easier
+	return true;
 }
 
 
