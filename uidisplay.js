93b506e6918ca7b70463a956561fd30065c3393a
Thomas Karpiniec <tk@1.21jiggawatts.net>
2015-09-12 20:37:52 +1000

    More WIP on UI display

--- uidisplay.js
+++ uidisplay.js
@@ -20,7 +20,7 @@ function updatePollUI() {
 		}
 	}
 	
-	// Start with initial values
+	// Start by setting initial values from the party objects
 	for (var i = 0; i < g.parties; i++) {
 		var party = g.parties[i];
 		for (var j = 0; j < party.initialIssueScores; j++) {
@@ -28,7 +28,6 @@ function updatePollUI() {
 			polls[i][j].push(score);
 		}
 	}
-	// Now we can rely on polls[party][issue][0] always being set as initial value
 	
 	// For each round, for each party, for each issue,
 	// apply all of the lobbies and bribes and set the next value
@@ -45,13 +44,34 @@ function updatePollUI() {
 		for (var b = 0; b < g.bribes.length; b++) {
 			var bribe = g.bribes[b];
 			if (bribe.round == r) {
-				var player = g.players[bribe.player];
-				
-				polls[bribe.party][bribe.issue][r] += 2;
+				polls[bribe.party][bribe.issue][r] += bribe.change;
+			}
+		}
+		for (var l = 0; l < g.lobbies.length; l++) {
+			var lobby = g.lobbies[l];
+			if (lobby.round == r) {
+				polls[lobby.party][lobby.issue][r] += lobby.change;
 			}
 		}
 	}
 	
-	
+	// In theory they're all correct now
+	// For each div, clear the existing content then redo the histogram creation
+	for (var p = 0; p < g.parties.length; p++) {
+		for (var i = 0; i < g.issues.length; i++) {
+			// Does the Player support this issue?
+			var supportsIssue = true;
+			for (var assigned = 0; assigned < g.currentPlayer.assignedIssues; assigned++) {
+				var assignedIssue = g.currentPlayer.assignedIssues[i];
+				if (assignedIssue.issue == i) {
+					supportsIssue = assignedIssue.inFavour;
+				}
+			}
+		
+			var divId = "histogram-party" + p + "-issue" + i;
+			$(divId).html("");
+			createHistogram(divId, polls[p][i], 6, !supportsIssue);
+		}
+	}
 }
 
